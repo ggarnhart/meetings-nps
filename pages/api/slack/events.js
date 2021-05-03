e4cd79aa563f9fd1633da30ae543d909784b4bfc
Greg Garnhart <greg.garnhart@sap.com>
2021-05-03 13:14:10 -0700

    challenge

--- pages/api/slack/events.js
+++ pages/api/slack/events.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  res.status(200).json({ data: "John Doe" });
+  res.status(200).json({ challenge: req.body.challenge });
 };
